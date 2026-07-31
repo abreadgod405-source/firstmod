19e2d8f2e0a89edf6e22248468fa304e06129831
RGLew <Abreadgod405@gmail.com>
2026-07-31 14:03:36 +0100

    Uhhh idk what im doing

---
+++ src/main/java/net/breadcraftsvids/firstmod/FirstMod.java
@@ -0,0 +1,16 @@
+package net.breadcraftsvids.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//important comment
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
