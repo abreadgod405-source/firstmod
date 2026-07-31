01f2116570440d1a0158e1fd8f2169ec4a00dc4a
RGLew <Abreadgod405@gmail.com>
2026-07-31 14:05:58 +0100

    Officially made a comment VERY important

--- src/main/java/net/breadcraftsvids/firstmod/FirstMod.java
+++ src/main/java/net/breadcraftsvids/firstmod/FirstMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//important comment
+// VERY important comment
 public class FirstMod implements ModInitializer {
 	public static final String MOD_ID = "firstmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
